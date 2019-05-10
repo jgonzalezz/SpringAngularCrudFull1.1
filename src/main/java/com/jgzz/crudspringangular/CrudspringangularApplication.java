f1eb154d1066a169c77935c69e400abadca3249e
jgzz <jgzz.dev@gmail.com>
2019-05-09 21:53:12 -0500

    First committ :)

---
+++ src/main/java/com/jgzz/crudspringangular/CrudspringangularApplication.java
@@ -0,0 +1,13 @@
+package com.jgzz.crudspringangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudspringangularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudspringangularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jgzz/crudspringangular/CrudspringangularApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jgzz.crudspringangular;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrudspringangularApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
