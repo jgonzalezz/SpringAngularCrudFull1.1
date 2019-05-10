cf0dab04b9af8eb28b5386b1ab6f37d76c276e3c
jgzz <jgzz.dev@gmail.com>
2019-05-09 22:31:40 -0500

    Index Controller

---
+++ src/main/java/com/jgzz/crudspringangular/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.jgzz.crudspringangular.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+
+    @GetMapping(value="/index")
+    public String index(){
+
+
+
+        return "index";
+    }
+
+
+
+}
